d443ffd2cfa25ebc4f8b146c8695e3a8f765176c
xEricL <37921711+xEricL@users.noreply.github.com>
2024-09-18 18:20:50 -0400

    try dev zig 14 why not

--- script/install_zig.sh
+++ script/install_zig.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -eux
 
-url="https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz"
-sha256="2d00e789fec4f71790a6e7bf83ff91d564943c5ee843c5fd966efc474b423047"
+#url="https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz"
+#sha256="2d00e789fec4f71790a6e7bf83ff91d564943c5ee843c5fd966efc474b423047"
+url="https://ziglang.org/builds/zig-linux-x86_64-0.14.0-dev.1587+feaee2ba1.tar.xz"
+sha256="1ef191a4eef1e17284d596c38ae4f6e797c1d33635f61086caca322dde3fb136"
 
 curl -sSL ${url} -o zig.tar.xz
 echo ${sha256} zig.tar.xz | sha256sum -c -
